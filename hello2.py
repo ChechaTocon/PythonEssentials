9bd6856fbe14aa57518a82b7bd6f072cf2f1aaeb
ChechaTocon <cesarchecha91@gmail.com>
2019-12-03 00:17:57 -0600

    no deberia

--- hello2.py
+++ hello2.py
@@ -1,3 +1,4 @@
 print("Creacion del repositorio para los ejemplos de python")
 #creacion de un comentario
 #probando el guardado de la contraseña
+#ahora no debaria de pedirme ni mi usuario ni la contraseña
