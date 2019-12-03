085136390266f9d4956e4a91c8ecd0d21a5a5e85
ChechaTocon <cesarchecha91@gmail.com>
2019-12-03 12:01:00 -0600

    Funcion mensaje

--- hello2.py
+++ hello2.py
@@ -1,5 +1,9 @@
 print("Creacion del repositorio para los ejemplos de python")
+print("Empezando con funciones")
 #creacion de un comentario
 #probando el guardado de la contraseña
 #ahora no debaria de pedirme ni mi usuario ni la contraseña
 #funcio pinches prros
+def mensaje():#entre los parentecis puede ir  parametros
+    print("Este es texto de una funcion")
+mensaje()
