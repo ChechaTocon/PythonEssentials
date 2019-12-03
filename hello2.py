10ce527c8dd8a5502f041f42c1fab576a6d1058c
ChechaTocon <cesarchecha91@gmail.com>
2019-12-03 00:18:17 -0600

    funciono

--- hello2.py
+++ hello2.py
@@ -2,3 +2,4 @@ print("Creacion del repositorio para los ejemplos de python")
 #creacion de un comentario
 #probando el guardado de la contraseña
 #ahora no debaria de pedirme ni mi usuario ni la contraseña
+#funcio pinches prros
